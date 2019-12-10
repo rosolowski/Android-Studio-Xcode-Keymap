9b37093aee7d5356e57db9590c154bb99951995d
Oskar Rosołowski <oskar.rosolowski@gmail.com>
2019-12-11 00:25:28 +0100

    Change paths to support Android Studio

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# find path for IntelliJ preferences directory (can be multiple)
-IFS=$'\n' read -rd '' -a I <<< $(find ~/Library/Preferences -type d -name IntelliJIdea* -o -type d -name IdeaIC*)
-# execute `curl` to save keymap
+# find path for AndroidStudio preferences directory (can be multiple)
+IFS=$'\n' read -rd '' -a I <<< $(find ~/Library/Preferences -type d -name "AndroidStudio*")
+
 for temp in $I; do
+        if [ ! -d "$temp/keymaps" ]; then
+          mkdir "$temp/keymaps"
+          echo "Creating folder keymaps in $temp"
+        fi
         X="$temp/keymaps/Keymap_Xcode.xml"
         echo Installing as $X
         cp ./Keymap_Xcode.xml "$X"
